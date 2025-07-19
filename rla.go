fb8af0667a4bdc18fea9c1cd6ddec1a547184ac2
Bruce Ediger <bediger8@gmail.com>
2025-07-19 07:34:12 -0600

    everything but the audit

--- rla.go
+++ rla.go
@@ -5,18 +5,22 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"strconv"
 	"strings"
 )
 
 type candidate struct {
 	name       string
-	proportion float64
+	percentage float64
+	votes      int
 }
 
 func main() {
 	candidateProportions := flag.String("c", "", "x:%,y:%(,z:%)")
 	lie := flag.Bool("z", false, "maybe lie about who won")
+	ballotCount := flag.Int("b", 1000, "count of ballots")
+	votingFineness := flag.Int("f", 1000, "voting fineness")
 	flag.Parse()
 
 	candidates, err := parseCandidates(*candidateProportions)
@@ -25,12 +29,17 @@ func main() {
 	}
 
 	for _, candidate := range candidates {
-		fmt.Printf("Candidate %s: %.02f%%\n", candidate.name, candidate.proportion)
+		fmt.Printf("Candidate %s: %.02f%%\n", candidate.name, candidate.percentage)
 	}
 
-	if *lie {
-		fmt.Printf("might lie about who won\n")
-	}
+	voting := createVoting(candidates, *votingFineness)
+
+	_ = createVotes(voting, *ballotCount, *votingFineness)
+
+	winner := countVotes(candidates, *ballotCount, *lie)
+	fmt.Printf("%s declared winner\n", winner)
+
+	// auditVotes(votes, *ballotCount)
 }
 
 func parseCandidates(candidateProportions string) ([]*candidate, error) {
@@ -49,12 +58,12 @@ func parseCandidates(candidateProportions string) ([]*candidate, error) {
 		if p, err := strconv.ParseFloat(g[1], 64); err == nil {
 			c := &candidate{
 				name:       g[0],
-				proportion: p,
+				percentage: p,
 			}
 			candidates = append(candidates, c)
 			sum += p
 		} else {
-			log.Printf("Candidate %q: %v\n", f, err)
+			log.Printf("# Candidate %q: %v\n", f, err)
 		}
 	}
 
@@ -63,4 +72,49 @@ func parseCandidates(candidateProportions string) ([]*candidate, error) {
 	}
 
 	return candidates, nil
+}
+
+func createVoting(candidates []*candidate, fineness int) []*candidate {
+
+	votes := make([]*candidate, fineness)
+	i := 0
+
+	factor := float64(fineness) / 100.0
+
+	for _, c := range candidates {
+		x := int(c.percentage * factor)
+		for ; x > 0; x-- {
+			votes[i] = c
+			i++
+		}
+	}
+
+	return votes
+}
+
+func createVotes(voting []*candidate, ballotCount int, fineness int) []string {
+
+	votes := make([]string, ballotCount)
+
+	for i := 0; i < ballotCount; i++ {
+		j := rand.Intn(fineness)
+		voting[j].votes++
+		votes[i] = voting[j].name
+	}
+
+	return votes
+}
+
+func countVotes(candidates []*candidate, ballotCount int, lie bool) string {
+
+	winner := candidates[0].name
+	votes := candidates[0].votes
+	for _, c := range candidates {
+		if c.votes > votes {
+			winner = c.name
+			votes = c.votes
+		}
+		fmt.Printf("%s\t%d\t%.02f\t%.02f\n", c.name, c.votes, c.percentage, float64(c.votes)/float64(ballotCount)*100.)
+	}
+	return winner
 }
